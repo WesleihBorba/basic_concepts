2b44e9e410e75b0e29e66196ff1a1d3068524547
Weslei Borba <weslei.bico123@gmail.com>
2025-07-28 13:11:15 -0300

    Dunder Methods

--- dunder_methods.py
+++ dunder_methods.py
@@ -3,19 +3,26 @@ import numpy as np
 
 
 class AnalyzeArrays:
-    def __init__(self, data_array):  # Initialize the values
-        self.array_origin = data_array
+
+    def __init__(self, data):  # Initialize the values
+        self.array_origin = np.asarray(data)
         self.transformed_array = []
 
     def __add__(self, other):  # Increase the values in a new array
-        self.transformed_array = self.array_origin + other.array_origin
-        return self.transformed_array
+        if isinstance(other, AnalyzeArrays):
+            if self.array_origin.shape != other.array_origin.shape:
+                raise ValueError("Arrays with different shapes cannot be added.")
+            return AnalyzeArrays(self.array_origin + other.array_origin)
+        else:
+            return AnalyzeArrays(self.array_origin + other)
 
     def __len__(self):  # Size of each data
         return len(self.array_origin)
 
     def __eq__(self, other):  # Know if the values are equals
-        return sum(self.array_origin) == sum(other.array_origin)
+        if not isinstance(other, AnalyzeArrays):
+            return NotImplemented
+        return np.array_equal(self.array_origin, other.array_origin)
 
     def __repr__(self) -> str:  # Show the values
         return f"{self.__class__.__name__}({self.array_origin!r})"
@@ -32,7 +39,5 @@ print(repr(sum_array))
 
 equals_values = analyze_1 == analyze_2
 size_values = len(analyze_1) == len(analyze_2)
-
-print(size_values)
-print(sum_array)
-print(equals_values)
+print(repr(equals_values))
+print(repr(size_values))
