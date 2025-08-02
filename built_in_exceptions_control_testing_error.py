ba46da3099aa428180eb315afae2e6643b3c6baf
Weslei Borba <weslei.bico123@gmail.com>
2025-08-02 17:49:16 -0300

    Testing error

--- built_in_exceptions_control_testing_error.py
+++ built_in_exceptions_control_testing_error.py
@@ -1,4 +1,5 @@
 # DEFINE THE GOALS MAYBE CLUSTER
+import unittest
 
 # Raise error:
 def open_register(employee_status):
@@ -110,4 +111,26 @@ instrument = 'Piano'
 quantity = 5
 submit_order(instrument, quantity)
 
+
+# UnitTest
+
+# Function that gets tested
+def times_ten(number):
+    return number * 100
+
+
+# Test class
+class TestTimesTen(unittest.TestCase):
+    def test_multiply_ten_by_zero(self):
+        self.assertEqual(times_ten(0), 0, 'Expected times_ten(0) to return 0')
+
+    def test_multiply_ten_by_one_million(self):
+        self.assertEqual(times_ten(1000000), 10000000, 'Expected times_ten(1000000) to return 10000000')
+
+    def test_multiply_ten_by_negative_number(self):
+        self.assertEqual(times_ten(-10), -100, 'Expected add_times_ten(-10) to return -100')
+
+
+# Run the tests
+unittest.main()
 
