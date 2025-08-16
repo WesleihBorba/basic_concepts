742b0c5758af55adcc5f01121578eb5a59d89ada
Weslei Borba <weslei.bico123@gmail.com>
2025-08-16 20:11:21 -0300

    Testing error

--- built_in_exceptions_control_testing_error.py
+++ built_in_exceptions_control_testing_error.py
@@ -1,46 +1,104 @@
 # We will provoke errors using Logistic regression
 import unittest
+
+import numpy as np
 from sklearn.datasets import load_breast_cancer
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score,classification_report,confusion_matrix,ConfusionMatrixDisplay
+from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 cancer_data = load_breast_cancer()
 X, y = cancer_data.data, cancer_data.target
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Standardize the numerical features
-scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
+class CreateErrorInDatabase:
+    @staticmethod
+    def transform_y_class(data_y):
+        data_y[data_y == 0] = 1
+        data_replaced_y = data_y.copy()
+        return data_replaced_y
 
-# Create a logistic regression model
-model = LogisticRegression()
+    @staticmethod
+    def add_nan_in_x(data_x):
+        data_x[1, 1:4] = [np.nan, np.nan, np.nan]
+        data_replaced_x = data_x.copy()
+        return data_replaced_x
 
-# Fit the model to the training data
-model.fit(X_train_scaled, y_train)
+    @staticmethod
+    def reshape_data_x(data_x):
+        data_reshape_x = data_x.reshape(-1)
+        return data_reshape_x
 
-# Make predictions on the test data, to test the model
-y_prediction = model.predict(X_test_scaled)
+    @staticmethod
+    def resize_data_x(data_x):
+        data_resize = np.resize(data_x, (565, 25))
+        return data_resize
 
-# Evaluate the model
-accuracy = accuracy_score(y_test, y_prediction)
-classification_report = classification_report(y_test,y_prediction)
-print(f'accuracy score: {accuracy}')
-print(f'Classification report : \n  {classification_report}')
 
-exit()
 # Creating a class to build logistic regression
-class LogisticRegressionAnalysis:
+class LogisticRegressionAnalysis(CreateErrorInDatabase):
     def __init__(self, x_data, y_data):
         self.x_data = x_data
         self.y_data = y_data
 
+    def analysis_data(self):
 
+        # Creating an error in data, changing a number of class (Create Error)
+        self.y_data = self.transform_y_class(self.y_data)
+
+        # Add Null values in X data (Create Error)
+        self.x_data = self.add_nan_in_x(self.x_data)
+
+        # Reshape data X (Create Error)
+        self.x_data = self.reshape_data_x(self.x_data)
+
+        # Resize and bring a error in X (Create Error)
+        self.x_data = self.resize_data_x(self.x_data)
+
+        # Split the data into training and testing sets
+        X_train, X_test, y_train, y_test = train_test_split(self.x_data, self.y_data, test_size=0.2,
+                                                            random_state=42)
+
+        # Standardize the numerical features
+        scaler = StandardScaler()
+        X_train_scaled = scaler.fit_transform(X_train)
+        X_test_scaled = scaler.transform(X_test)
+
+        # Create a logistic regression model
+        model = LogisticRegression()
+
+        # Fit the model to the training data
+        model.fit(X_train_scaled, y_train)
+
+        # Make predictions on the test data, to test the model
+        y_prediction = model.predict(X_test_scaled)
+
+        # Evaluate the model
+        accuracy = accuracy_score(y_test, y_prediction)
+        classification_report_data = classification_report(y_test, y_prediction)
+        print(f'accuracy score: {accuracy}')
+        print(f'Classification report : \n  {classification_report_data}')
+
+
+class_logistic = LogisticRegressionAnalysis(X, y)
+class_logistic.analysis_data()
+
+exit()
 # Testing Number of classes
+
+# NaN values
+
+# Wrong Dimension
+
+# X and Y different
+
+
+
+
+class TestingError:
+    pass
+
 
 # Raise error:
 def open_register(employee_status):
