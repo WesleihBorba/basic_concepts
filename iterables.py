9ec7003a560b382f8a767689349aec856290af72
Weslei Borba <weslei.bico123@gmail.com>
2025-08-18 21:49:13 -0300

    Iterables code

--- iterables.py
+++ iterables.py
@@ -1,10 +1,25 @@
 # Iterable in big data
 sku_list = [7046538, 8289407, 9056375, 2308597]
 
-sku_iterator_object_one = sku_list.__iter__()
-sku_iterator_object_two = iter(sku_list)  # Return an object list iterator
 
-for sku in range(0, len(sku_list)):
-    next_sku = sku_iterator_object_one.__next__()
-    print(next_sku)
+class IterableBigData:
+    def __init__(self, data):
+        self.big_data = data
 
+    def __iter__(self):
+        self.index = 0
+        return self
+
+    def __next__(self):
+        if self.index < len(self.big_data):
+            self.index += 1
+            return self.big_data[self.index - 1]
+        else:
+            raise StopIteration
+
+
+iterable_counter = IterableBigData(sku_list)
+for i in iterable_counter:
+    print(i)
+
+# PAREI AQUI: Python’s Itertools: Built-in Iterators
