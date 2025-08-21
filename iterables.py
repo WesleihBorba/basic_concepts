0a2b569cdaf79c2634d326d59a889c698bcf7a60
Weslei Borba <weslei.bico123@gmail.com>
2025-08-21 10:22:52 -0300

    Iterables code

--- iterables.py
+++ iterables.py
@@ -29,3 +29,79 @@ for i in iterable_counter:
 # next()
 # Generator comprehension - Need to use loop to access
 a_generator = (i*i for i in range(4))
+
+# Method send(), pensar em como usar
+def count_generator():
+  while True:
+    n = yield
+    print(n)
+
+my_generator = count_generator()
+next(my_generator) # 1st Iteration Output:
+next(my_generator) # 2nd Iteration Output: None
+my_generator.send(3) # 3rd Iteration Output: 3
+next(my_generator) # 4th Iteration Output: None
+
+# throw(), também pensar em algo
+def generator():
+  i = 0
+  while True:
+    yield i
+    i += 1
+
+my_generator = generator()
+for item in my_generator:
+    if item == 3:
+        my_generator.throw(ValueError, "Bad value given")
+
+
+# Method close too
+def generator():
+  i = 0
+  while True:
+    yield i
+    i += 1
+
+my_generator = generator()
+next(my_generator)
+next(my_generator)
+my_generator.close()
+next(my_generator) # raises StopGenerator exception
+
+# Connection generators
+def cs_courses():
+    yield 'Computer Science'
+    yield 'Artificial Intelligence'
+
+def art_courses():
+    yield 'Intro to Art'
+    yield 'Selecting Mediums'
+
+
+def all_courses():
+    yield from cs_courses()
+    yield from art_courses()
+
+combined_generator = all_courses()
+
+# Generator Pipelines
+
+def number_generator():
+    i = 0
+    while True:
+        yield i
+        i += 1
+
+
+def even_number_generator(numbers):
+    for n in numbers:
+        if n % 2 == 0:
+            yield n
+
+
+even_numbers = even_number_generator(number_generator())
+
+for e in even_numbers:
+    print(e)
+    if e == 100:
+        break
