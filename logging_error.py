dca7d13df4659f869906eec6ed986dc3db530484
Weslei Borba <weslei.bico123@gmail.com>
2025-09-05 23:44:13 -0300

    Logging

--- logging_error.py
+++ logging_error.py
@@ -3,12 +3,19 @@ import logging
 import sys
 
 # Logger setting
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 stream_handler = logging.StreamHandler(sys.stdout)
 formatter = logging.Formatter('[%(asctime)s] %(levelname)s [%(message)s]')
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
 logger.setLevel(logging.INFO)
+
+# Class cluster
+class DiabeticCluster:
+    pass
+
+# https://medium.com/@dKimothi/finding-best-features-for-predicting-diabetes-8656cf0d1185
 
 # TRAINING LOGS
 
