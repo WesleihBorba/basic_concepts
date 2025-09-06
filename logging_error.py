31b93077f029be0374304377e8cc32196f59088e
Weslei Borba <weslei.bico123@gmail.com>
2025-09-06 14:04:02 -0300

    Logging

--- logging_error.py
+++ logging_error.py
@@ -1,6 +1,10 @@
 # Goal: We will use logging to monitor the application of our cluster
 import logging
 import sys
+from sklearn import datasets, linear_model
+import numpy as np
+from sklearn.metrics import mean_squared_error, r2_score
+
 
 # Logger setting
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -11,9 +15,41 @@ stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
 logger.setLevel(logging.INFO)
 
+
 # Class cluster
 class DiabeticCluster:
-    pass
+    def __init__(self):
+        self.diabetes_X, self.diabetes_y = datasets.load_diabetes(return_X_y=True)
+        self.y_train = []
+        self.y_test = []
+        self.X_train = []
+        self.X_test = []
+        self.regression = []
+
+    def create_data(self):
+        # Split the data into training/testing sets
+        self.X_train = self.diabetes_X[:-20]
+        self.X_test = self.diabetes_X[-20:]
+
+        # Split the targets into training/testing sets
+        self.y_train = self.diabetes_y[:-20]
+        self.y_test = self.diabetes_y[-20:]
+
+    def train(self):
+        # Create linear regression object
+        self.regression = linear_model.LinearRegression()
+
+        # Train the model using the training sets
+        self.regression.fit(self.X_train, self.y_train)
+
+    def predict(regr, X_test):
+        # Make predictions using the testing set
+        diabetes_y_pred = regr.predict(X_test)
+
+        return diabetes_y_pred
+
+    def eval(y, y_hat):
+        return mean_squared_error(y, y_hat)
 
 # https://medium.com/@dKimothi/finding-best-features-for-predicting-diabetes-8656cf0d1185
 
