c903764de5799f81fb8fcc747a2dba648ad2d33f
Weslei Borba <weslei.bico123@gmail.com>
2025-09-13 00:21:11 -0300

    Threats

--- concurrent_programming.py
+++ concurrent_programming.py
@@ -1,7 +1,25 @@
 # Goal:
+import threading
+import time
+
+def greeting_with_sleep(string):
+  s = time.perf_counter()
+  print(string)
+  time.sleep(2)
+  print("says hello!")
+  elapsed = time.perf_counter() - s
+  print("Sequential Programming Elapsed Time: " + str(elapsed) + " seconds")
+
+greeting_with_sleep('Codecademy')
+
+t = threading.Thread(target=greeting_with_sleep, args=('Codecademy',))
+t.start()
+
 
 # Concurrency
 
 # Parallelism
 
 # asynchronous
+
+# PAREI AQUI: The Asyncio Module
