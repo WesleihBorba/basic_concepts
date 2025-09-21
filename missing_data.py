bf28cf406814b40dddf04ed441091e96454b24c5
Weslei Borba <weslei.bico123@gmail.com>
2025-09-20 23:44:27 -0300

    Missing Data

---
+++ missing_data.py
@@ -0,0 +1,15 @@
+# Goal: Treat data with missing values
+import pandas as pd
+
+
+class MissingData:
+
+    def __init__(self):
+        self.data_missing = pd.DataFrame()
+
+    def read_file(self):
+        pass
+
+
+
+# PAREI AQUI: Missing at Random (MAR)
