5a7cd56961afb9bf459b6b54c6c040326f5be78a
Weslei Borba <weslei.bico123@gmail.com>
2025-09-21 20:23:15 -0300

    Missing Data

--- missing_data.py
+++ missing_data.py
@@ -18,31 +18,46 @@ logger.addHandler(stream_handler)
 class MissingData:
 
     def __init__(self):
-        self.data_missing = pd.DataFrame()
-
-    def read_file(self):
         logger.info('Reading a missing file data')
+        self.data_missing = pd.read_csv('files\\missing_file.csv', sep=';')
 
     def locf_missing(self):
         logger.info('Add last observation carried forward')
-        self.data_missing['comfort'].ffill(axis=0, inplace=True)
+
+        logger.info(f'Before to change our data: {self.data_missing['value_stock']}')
+        self.data_missing['value_stock'] = self.data_missing['value_stock'].ffill(axis=0)
+        logger.info(f'Data after change: {self.data_missing}')
 
     def nocb_missing(self):
         logger.info('Add next observation carried backward')
-        self.data_missing['comfort'].bfill(axis=0, inplace=True)
+
+        logger.info(f'Before to change our data: {self.data_missing['maximum_stock']}')
+        self.data_missing['maximum_stock'] = self.data_missing['maximum_stock'].bfill(axis=0)
+        logger.info(f'Data after change: {self.data_missing}')
 
     def other_methods(self):
         logger.info('Filling with the first value of our dataframe')
-        first_value = self.data_missing['data'][0]
-        self.data_missing['concentration'].fillna(value=first_value, inplace=True)
+        logger.info(f'Before to change our data: {self.data_missing['volume']}')
+        first_value = self.data_missing['volume'][0]
+        self.data_missing['volume'] = self.data_missing['volume'].fillna(value=first_value)
+        logger.info(f'Data after change: {self.data_missing}')
 
         logger.info('Filling with the worst value, minimal ou maximum, depends of our data')
-        worst = self.data_missing['pain'].max()
-        self.data_missing['pain'].fillna(value=worst, inplace=True)
+        worst = self.data_missing['minimum_stock'].min()
+        logger.info(f'Before to change our data: {self.data_missing['minimum_stock']}')
+        self.data_missing['minimum_stock'] = self.data_missing['minimum_stock'].fillna(value=worst)
+        logger.info(f'Data after change: {self.data_missing}')
 
+    def drop_missing_data(self):
+        logger.info('Dropping missing data, the last alternative')
 
-
-    def drop(self):
+        logger.info(f'Before to change our data: {self.data_missing}')
         self.data_missing.dropna(inplace=True)
+        logger.info(f'Data after change: {self.data_missing}')
 
-# PAREI AQUI: Missing at Random (MAR)
+
+new_class = MissingData()
+new_class.locf_missing()
+new_class.nocb_missing()
+new_class.other_methods()
+new_class.drop_missing_data()
