63f2e06f5a0bb961f2742bb5527e9ec74e18530c
Weslei Borba <weslei.bico123@gmail.com>
2025-09-21 15:49:51 -0300

    Missing Data

--- missing_data.py
+++ missing_data.py
@@ -1,5 +1,18 @@
 # Goal: Treat data with missing values
 import pandas as pd
+import logging
+import sys
+
+# Logger setting
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)  # Console will show everything
+
+# Handler to console
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('[%(asctime)s] %(levelname)s - %(message)s')
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
 
 
 class MissingData:
@@ -10,6 +23,9 @@ class MissingData:
     def read_file(self):
         pass
 
+    def locf_missing(self):
+        logger.info('Add last observation carried forward')
+        self.data_missing['comfort'].ffill(axis=0, inplace=True)
 
 
     def drop(self):
