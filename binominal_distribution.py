e7b4e72fb710a9132c571017c00e91f96330da23
Weslei Borba <weslei.bico123@gmail.com>
2025-10-02 12:24:48 -0300

    Binominal Distribution

--- binominal_distribution.py
+++ binominal_distribution.py
@@ -1,2 +1,14 @@
 # Goal:
+import numpy as np
+import matplotlib.pyplot as plt
 
+
+# Let's generate 10,000 "experiments"
+# N = 10 shots
+# P = 0.30 (30% he'll get a free throw)
+
+a = np.random.binomial(10, 0.30, size=10000)
+plt.hist(a, range=(0, 10), bins=10)
+plt.xlabel('Number of "Free Throws"')
+plt.ylabel('Frequency')
+plt.show()
