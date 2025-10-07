306cded8495af93f16e38704491d212847b065da
Weslei Borba <weslei.bico123@gmail.com>
2025-10-06 21:10:38 -0300

    Linear Regression

---
+++ linear_regression.py
@@ -0,0 +1,7 @@
+# Goal:
+
+# Introduction to Linear Regression with sklearn and gradient
+
+# Linear Regression with a Categorical Predictor
+
+# Matrix Representation of Linear Regression: Understand the math and new libraries
