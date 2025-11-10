09dedfabcbeef85acc6e146369785be756346674
Weslei Borba <weslei.bico123@gmail.com>
2025-11-10 20:52:41 -0300

    Poly regression

---
+++ polynomial_regression.py
@@ -0,0 +1,19 @@
+# Goal:
+import statsmodels.api as sm
+
+## USAR MAKE REGRESSION E ALGUNS CALCULOS PARA FAZER A LINHA DE REGRESSÃO COM 1 OU 2 VARIÁVEIS
+
+##  Why We Need Interaction and Polynomial Terms: Relations of features
+# Fit model2 regression here:
+model2 = sm.OLS.from_formula('height ~ weight + species + weight:species', data=plants).fit() # Entender como funcionar o weight:species
+# Print model2 results here:
+print(model2.params)
+
+
+
+
+## Log Transformations (And More)
+
+## E assumptions pegar em sites
+
+## Criar da mesma forma que foi feito nos demais
