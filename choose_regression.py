05fd91251b7517942195856957cc689a384f466e
Weslei Borba <weslei.bico123@gmail.com>
2025-11-27 21:55:52 -0300

    Distance

--- choose_regression.py
+++
@@ -1,7 +0,0 @@
-# Goal:
-
-
-
-# CHOOSING A LINEAR REGRESSION MODEL
-# Adjusted R-Squared
-# Entender sobre overfitting e underfitting mais a fundo
---
+++ distances.py
@@ -0,0 +1,5 @@
+# Goal:
+
+
+
+# Representing Points: types of distance
