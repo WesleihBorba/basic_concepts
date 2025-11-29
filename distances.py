d9c85345840687dc9bf5140d6bcc062b913e6bbb
Weslei Borba <weslei.bico123@gmail.com>
2025-11-29 19:58:50 -0300

    Distance

--- distances.py
+++ distances.py
@@ -1,5 +1,18 @@
 # Goal:
+from scipy.spatial import distance
 
 
 
 # Representing Points: types of distance
+
+## Euclidean Distance -- No cálculo de vizinhos (KNN) -- Na predição
+print(distance.euclidean([1, 2], [4, 0]))
+
+## Manhattan Distance -- Lasso Regression -- Na função de custo quando usa penalização L1
+print(distance.cityblock([1, 2], [4, 0]))
+
+## Hamming Distance -- K-Modes (clustering para dados categóricos)
+print(distance.hamming([5, 4, 9], [1, 7, 9]))
+
+## Temtar usar para um exemplo assim: Distance functions are often used as error or cost functions to be minimized in an optimization problem.
+
