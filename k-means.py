54204ec17f9aab2fed7acaa029f4b0f76ad867b2
Weslei Borba <weslei.bico123@gmail.com>
2026-01-18 00:14:32 -0300

    K Means

---
+++ k-means.py
@@ -0,0 +1,70 @@
+# Goal: Segmentation Supermarket mall customers. Understanding more our customers and discover "target customers'
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets
+from copy import deepcopy
+from sklearn.cluster import KMeans
+import logging
+import sys
+
+# Logger setting
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)  # Console will show everything
+
+# Handler to console
+stream_handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter('[%(asctime)s] %(levelname)s - %(message)s')
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+
+# https://www.kaggle.com/datasets/vjchoudhary7/customer-segmentation-tutorial-in-python
+class KMeansClassifier:
+    def __init__(self):
+        pass
+
+    def normalization(self):
+        pass
+
+    def train_test(self):
+        pass
+
+    def fitting_data(self):
+        # USAR ELBOW / Sillhouette Best K
+        pass
+
+    def predict_model(self):
+        pass
+
+    def evaluating_model(self):
+        pass
+
+    def plot_groups(self):
+        # USAR Scatter Plots para distinguir grupos
+        pass
+
+
+
+exit()
+
+centroids = np.array(list(zip(centroids_x, centroids_y)))
+
+model = KMeans(init="k-means++", n_clusters=2)
+# Initial centroids
+# plt.scatter(centroids[:, 0], centroids[:, 1], marker='D', s=100)
+
+results = model.fit_predict(values)
+
+plt.scatter(x, y, c=results, alpha=1)
+
+# Cluster centers
+plt.scatter(model.cluster_centers_[:, 0], model.cluster_centers_[:, 1], marker='v', s=100)
+
+ax = plt.subplot()
+ax.set_xticks([0, 1, 2, 3, 4, 5])
+ax.set_yticks([0, 1, 2, 3, 4])
+
+plt.title('K-Means++ Initialization')
+plt.show()
+print("The model's inertia is " + str(model.inertia_))
+
+
