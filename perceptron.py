0c12eada5c8d87329b55d983572493233cfb1026
Weslei Borba <weslei.bico123@gmail.com>
2026-01-20 21:53:26 -0300

    Perceptron

---
+++ perceptron.py
@@ -0,0 +1,100 @@
+# Goal:
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from sklearn.preprocessing import StandardScaler
+import logging
+import sys
+
+# Logger setting
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)  # Console will show everything
+
+# Handler to console
+stream_handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter('[%(asctime)s] %(levelname)s - %(message)s')
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+
+
+
+
+
+
+# https://www.kaggle.com/datasets/basu369victor/prediction-of-asteroid-diameter
+# Talvez não fazer ou fazer usando K_nearest-neightbours_classifier para spam OU Aprovar crédito vs negar, tem no decision tree
+# https://chatgpt.com/c/69702242-054c-8328-a96b-955826480290
+
+
+
+
+
+
+class PerceptronClassifier:
+    def __init__(self):
+        self.normalization_data, self.scaler = None, None
+        self.model, self.predict_values = None, None
+
+        self.data = pd.read_csv('files\\Mall_Customers.csv')
+
+    def transform_data(self):
+        logger.info('Transforming Gender in 1 and 0')
+        map_gender = {'Male': 1, 'Female': 0}
+        self.data['Gender'] = self.data['Gender'].map(map_gender)
+        logger.debug(f'Changes of values, Mall Customers: gender{map_gender}')
+
+        self.data.drop(columns={'CustomerID', 'Gender'}, inplace=True)
+        self.data.rename(columns={'Annual Income (k$)': 'annual_income', 'Spending Score (1-100)': 'spending_score'},
+                         inplace=True)
+
+    def normalization(self):
+        logger.info('Normalization values')
+        self.scaler = StandardScaler()
+        self.normalization_data = pd.DataFrame(self.scaler.fit_transform(self.data), columns=self.data.columns)
+
+    def elbow_silhouette_method(self):
+        logger.info('Elbow: how close the points are within the same cluster')
+        logger.info('Silhouette: how far apart the clusters are from each other')
+        inertia = []
+        sil_scores = []
+        k_range = range(2, 11)
+
+        for k in k_range:
+            kmeans = KMeans(n_clusters=k, random_state=42).fit(self.normalization_data)
+            inertia.append(kmeans.inertia_)
+            sil_scores.append(silhouette_score(self.normalization_data, kmeans.labels_))
+
+        fig, ax1 = plt.subplots(figsize=(6, 4))
+
+        # Primary y-axis: Inertia (Elbow Method)
+        ax1.plot(k_range, inertia, 'bo-', label='Inertia (Elbow)')
+        ax1.set_xlabel('Number of Clusters k')
+        ax1.set_ylabel('Inertia', color='b')
+        ax1.tick_params(axis='y', labelcolor='b')
+
+        # Secondary y-axis: Silhouette Score
+        ax2 = ax1.twinx()
+        ax2.plot(k_range, sil_scores, 'go-', label='Silhouette Score')
+        ax2.set_ylabel('Silhouette Score', color='g')
+        ax2.tick_params(axis='y', labelcolor='g')
+
+        # Title and layout
+        plt.title('Elbow Method vs. Silhouette Score')
+        fig.tight_layout()
+        plt.show()
+
+    def fitting_data(self):
+        logger.info('Fitting with KMeans ++')
+        self.model = KMeans(
+            n_clusters=5,
+            init='k-means++',
+            random_state=42
+        ).fit(self.normalization_data)
+
+
+classification = PerceptronClassifier()
+classification.transform_data()
+classification.normalization()
+classification.elbow_silhouette_method()
+classification.fitting_data()
