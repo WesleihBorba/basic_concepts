06c864c6fc77843c6bcb742b6899e66504d027da
Weslei Borba <weslei.bico123@gmail.com>
2026-02-04 20:50:39 -0300

    Filter Methods

---
+++ filter_features.py
@@ -0,0 +1,15 @@
+# Goal: To learn some filtering methods for regression or classification models
+
+
+class FilterMethods:
+
+    def variance_threshold(self):
+        pass
+
+
+
+
+regression_data = None
+classification_data = None
+class_filter = FilterMethods()
+
