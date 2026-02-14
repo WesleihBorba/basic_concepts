70ddc6ed0a799d994b42ab025d6571b02bb30ae2
Weslei Borba <weslei.bico123@gmail.com>
2026-02-14 20:36:40 -0300

    Filter Methods

--- feature_selection.py
+++ feature_selection.py
@@ -3,6 +3,7 @@ from sklearn.feature_selection import (VarianceThreshold, f_regression, SelectKB
                                        mutual_info_regression, RFE, SequentialFeatureSelector)
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import make_regression, make_classification
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
@@ -22,9 +23,34 @@ logger.addHandler(stream_handler)
 
 
 class FilterMethods:
-    def __init__(self, reg_data, class_data):
-        self.regression_data = reg_data
-        self.classification_data = class_data
+    def __init__(self):
+        logger.info("It's an example, not represent data in real life")
+        self.X_reg, self.y_reg = make_regression(
+            n_samples=20000,
+            n_features=12,
+            noise=10,
+            random_state=42)
+        columns_regression = ["education", "working_time", "age", "experience_years", "job_level", "company_size",
+                              "city_tier", "technical_skills", "soft_skills", "industry_sector", "previous_salary",
+                              "english_level"]
+        self.regression_data = pd.DataFrame(self.X_reg, columns=columns_regression)
+        self.regression_data['no_variation'] = 50
+        self.regression_data['income'] = self.y_reg
+
+        self.X_class, self.y_class = make_classification(
+            n_samples=20000,
+            n_features=12,
+            n_redundant=0,
+            n_clusters_per_class=1,
+            class_sep=2,
+            random_state=42
+        )
+        columns_classification = ["income", "credit_score", "debt_to_income_ratio", "employment_history",
+                                  "loan_amount", "loan_term", "previous_defaults", "savings_balance", "property_value",
+                                  "number_of_open_credits", "age", "residence_type"]
+        self.classification_data = pd.DataFrame(self.X_class, columns=columns_classification)
+        self.classification_data["approved"] = self.y_class
+
         self.scaler, self.normalization_regression, self.normalization_classification = None, None, None
 
     def normalization(self):
@@ -33,28 +59,41 @@ class FilterMethods:
                     "Don't normalization all dataset, i'm using to show filters methods examples (not real life)")
         self.scaler = StandardScaler()
         self.normalization_regression = pd.DataFrame(self.scaler.fit_transform(self.regression_data),
-                                                     columns=self.regression_data .columns)
-        self.normalization_classification = pd.DataFrame(self.scaler.fit_transform(self.regression_data),
-                                                         columns=self.normalization_classification .columns)
+                                                     columns=self.regression_data.columns)
+        self.normalization_classification = pd.DataFrame(self.scaler.fit_transform(self.classification_data),
+                                                         columns=self.classification_data.columns)
 
     def variance_threshold(self):
         logger.info('Use variance to select features, drop low variance, will not have changes in our models')
-        selector = VarianceThreshold(threshold=0.2)
-        df_reduced = selector.fit_transform(self.normalization_regression)
-        selected_cols = self.normalization_regression.columns[selector.get_support()]
+        X = self.normalization_regression.drop(columns=['income'])
+        y = self.normalization_regression['income']
+        selector = VarianceThreshold(threshold=0.8)
+
+        selector.fit(X)
+        selected_cols = X.columns[selector.get_support()]
+        df_reduced = X[selected_cols].copy()
         logger.debug(f"Origin columns: {list(self.normalization_regression.columns)}")
         logger.debug(f"columns that remained: {list(selected_cols)}")
-        return df_reduced
 
-    def correlation(self):
-        logger.info('Look correlation of each dependency variables in regression (multicollinearity)')
-        corr_matrix = self.regression_data.corr().abs()
-        upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
-        to_drop = [column for column in upper.columns if any(upper[column] > 0.85)]
+    def correlation(self, target_column='income', threshold=0.3):
+        logger.info(f'Filtering features based on correlation with {target_column}')
+
+        corr_matrix = self.regression_data.corr()
+        target_corr = corr_matrix[target_column].abs()
+        low_corr_features = target_corr[target_corr >= threshold].index.tolist()
+
+        logger.info('Drop features with high correlation (Multicollinearity)')
+        abs_corr_matrix = corr_matrix.abs()
+        upper = abs_corr_matrix.where(np.triu(np.ones(abs_corr_matrix.shape), k=1).astype(bool))
+        redundant_features = [column for column in upper.columns if any(upper[column] > 0.85)]
+
+        to_drop = list(set(low_corr_features + redundant_features))
+        if target_column in to_drop:
+            to_drop.remove(target_column)
+
         df_reduced = self.regression_data.drop(columns=to_drop)
-        logger.debug(f"Removing columns with 0.85 of correlation in regression: {to_drop}")
-        logger.info("You could use correlation to compare with target variable, then you'll keep this variables")
-        return df_reduced
+        logger.debug(f"Origin columns: {list(self.regression_data.columns)}")
+        logger.debug(f"columns that remained: {list(df_reduced.columns)}")
 
     def test_f_regression(self):
         logger.info('Use to know relation between dependence variable and independence variable in regression')
@@ -176,25 +215,7 @@ class FilterMethods:
         print(feature_important) # Trocar para o dataframe correto
 
 
-# Criar os dados
-regression_data = None
-#self.X, self.y = make_regression( # COLOCAR UMA VARIÁVEL COM POUCA VARIÂNCIA OU APENAS UM VALOR COMO 1 OU 2 FIXO (PARA VARIANCIA)
-#    n_samples=15000,
-#    n_features=2,
-#    noise=10,
-#    random_state=42)
-#self.data = pd.DataFrame(self.X, columns=["education", "working_time"])
-#self.data['income'] = self.y
-classification_data = None
-#self.X, self.y = make_classification(
-#    n_samples=15000,
-#    n_features=2,
-#    n_redundant=0,
-#    n_clusters_per_class=1,
-#    class_sep=2,
-#    random_state=42
-#)
-#self.data = pd.DataFrame(self.X, columns=["income", "credit_score"])
-#self.data["approved"] = self.y
 class_filter = FilterMethods()
-#class_filter.variance_threshold()
+class_filter.normalization()
+class_filter.variance_threshold()
+class_filter.correlation()
