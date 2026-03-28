617c8cb0edb779cad7cfb77afdf267c782187f26
Weslei Borba <weslei.bico123@gmail.com>
2026-03-28 13:58:09 -0300

    Hyperparameter

--- choose_hyperparameters.py
+++ choose_hyperparameters.py
@@ -3,8 +3,8 @@ import pandas as pd
 import time
 from sklearn.model_selection import train_test_split, StratifiedKFold, GridSearchCV, RandomizedSearchCV
 from sklearn.ensemble import RandomForestClassifier
-from skopt import BayesSearchCV
-from skopt.space import Integer, Categorical
+import optuna
+from sklearn.model_selection import cross_val_score
 import logging
 import sys
 
@@ -22,7 +22,23 @@ logger.addHandler(stream_handler)
 class Hyperparameters:
     def __init__(self):
         self.X_train, self.X_test, self.y_train, self.y_test = [None] * 4
-        self.model, self.predict_values = [None] * 2
+        self.model_grid, self.model_random, self.model_bayesian, self.model_genetic = [None] * 4
+        self.cv = StratifiedKFold(
+            n_splits=5,
+            shuffle=True,
+            random_state=0
+        )
+
+        self.parameters = {
+            'n_estimators': [100, 200, 300],
+            'max_depth': [None, 5, 10, 15],
+            'min_samples_split': [2, 5, 10],
+            'min_samples_leaf': [1, 2, 5],
+            'max_features': ['sqrt', 'log2'],
+            'class_weight': [None, 'balanced']
+        }
+
+        self.algorithm = RandomForestClassifier(random_state=0, n_jobs=-1, criterion='gini', bootstrap=True)
 
         self.data = pd.read_csv('C:\\Users\\Weslei\\Desktop\\Assuntos_de_estudo\\Assuntos_de_estudo\\Fases da vida\\Fase I\\Repository Projects\\files\\bankloan.csv')
 
@@ -43,26 +59,12 @@ class Hyperparameters:
         logger.info('Finding best hyperparameters with Grid Search')
 
         start_time = time.perf_counter()
-        param_grid = {
-            'n_estimators': [100, 200, 300],
-            'max_depth': [None, 5, 10, 15],
-            'min_samples_split': [2, 5, 10],
-            'min_samples_leaf': [1, 2, 5],
-            'max_features': ['sqrt', 'log2'],
-            'class_weight': [None, 'balanced']
-        }
-
-        cv = StratifiedKFold(
-            n_splits=5,
-            shuffle=True,
-            random_state=0
-        )
 
         grid = GridSearchCV(
-            estimator=RandomForestClassifier(random_state=0, n_jobs=-1, criterion='gini', bootstrap=True),
-            param_grid=param_grid,
+            estimator=self.algorithm,
+            param_grid=self.parameters,
             scoring='recall',
-            cv=cv,
+            cv=self.cv,
             n_jobs=-1
         )
 
@@ -71,37 +73,23 @@ class Hyperparameters:
         logger.info(f'Best parameters: {grid.best_params_}')
         logger.info(f'Best CV score: {grid.best_score_:.4f}')
 
-        self.model = grid.best_estimator_
+        self.model_grid = grid.best_estimator_
         end_time = time.perf_counter()
 
         execution_time = end_time - start_time
         logger.debug(f"Program executed in: {execution_time:.5f} seconds")
-        logger.debug(f'Best Estimator of Grid Search: {self.model}')
+        logger.debug(f'Best Estimator of Grid Search: {self.model_grid}')
 
     def random_search(self):
         logger.info('Finding best hyperparameters with Random Search')
 
         start_time = time.perf_counter()
-        param_grid = {
-            'n_estimators': [100, 200, 300],
-            'max_depth': [None, 5, 10, 15],
-            'min_samples_split': [2, 5, 10],
-            'min_samples_leaf': [1, 2, 5],
-            'max_features': ['sqrt', 'log2'],
-            'class_weight': [None, 'balanced']
-        }
-
-        cv = StratifiedKFold(
-            n_splits=5,
-            shuffle=True,
-            random_state=0
-        )
 
         grid = RandomizedSearchCV(
-            estimator=RandomForestClassifier(random_state=0, n_jobs=-1, criterion='gini', bootstrap=True),
-            param_distributions=param_grid,
+            estimator=self.algorithm,
+            param_distributions=self.parameters,
             scoring='recall',
-            cv=cv,
+            cv=self.cv,
             n_jobs=-1
         )
 
@@ -110,32 +98,60 @@ class Hyperparameters:
         logger.info(f'Best parameters: {grid.best_params_}')
         logger.info(f'Best CV score: {grid.best_score_:.4f}')
 
-        self.model = grid.best_estimator_
+        self.model_random = grid.best_estimator_
         end_time = time.perf_counter()
 
         execution_time = end_time - start_time
         logger.debug(f"Program executed in: {execution_time:.5f} seconds")
-        logger.info(f'Best Estimator of Random Search: {self.model}')
+        logger.debug(f'Best Estimator of Random Search: {self.model_random}')
 
     def bayesian_optimization(self):
-        search_space = {
-            'n_estimators': Integer(50, 200),
-            'max_depth': Integer(1, 20),
-            'criterion': Categorical(['gini', 'entropy']),
-        }
+        logger.info('Finding best hyperparameters with Bayesian optimization')
 
-        opt = BayesSearchCV(
-            estimator=model,
-            search_spaces=search_space,
-            n_iter=50,  # Number of hyperparameter combinations to try
-            cv=StratifiedKFold(n_splits=5, shuffle=True, random_state=42),
-            scoring='accuracy',
-            n_jobs=-1,  # Use all available cores
-            random_state=42,
-        )
+        def objective(trial):
+            params = {
+                'n_estimators': trial.suggest_int('n_estimators', 50, 200),
+                'max_depth': trial.suggest_int('max_depth', 1, 20),
+                'min_samples_split': trial.suggest_int('min_samples_split', 2, 10),
+                'min_samples_leaf': trial.suggest_int('min_samples_leaf', 1, 5),
+                'max_features': trial.suggest_categorical('max_features', ['sqrt', 'log2']),
+                'class_weight': trial.suggest_categorical('class_weight', [None, 'balanced']),
+            }
 
+            clf = RandomForestClassifier(**params, random_state=0, n_jobs=-1, criterion='gini', bootstrap=True)
+
+            score = cross_val_score(clf, self.X_train, self.y_train,
+                                    cv=self.cv, scoring='recall', n_jobs=-1).mean()
+            return score
+
+        start_time = time.perf_counter()
+
+        optuna.logging.set_verbosity(optuna.logging.WARNING)
+        study = optuna.create_study(direction='maximize')
+        study.optimize(objective, n_trials=50)
+
+        logger.info(f'Best parameters: {study.best_params}')
+        logger.info(f'Best CV score: {study.best_value:.4f}')
+
+        self.model_bayesian = RandomForestClassifier(**study.best_params, random_state=0, n_jobs=-1, criterion='gini',
+                                                     bootstrap=True)
+        self.model_bayesian.fit(self.X_train, self.y_train)
+
+        end_time = time.perf_counter()
+
+        execution_time = end_time - start_time
+        logger.debug(f"Program executed in: {execution_time:.5f} seconds")
+        logger.debug(f'Best Estimator of Random Search: {self.model_bayesian}')
+
+
+    def genetic_algorithm(self):
+        pass
+    
+    def evaluating_model(self):
+        pass
 
 hyperparameters_class = Hyperparameters()
 hyperparameters_class.train_test()
 #hyperparameters_class.grid_search()
 #hyperparameters_class.random_search()
+hyperparameters_class.bayesian_optimization()
